2b4a3df067ebb1733e676a23328c3f485028499b
De Sean Ward <d.ward.chi@gmail.com>
2023-08-28 16:07:56 -0500

    All my files created

---
+++ server.js
@@ -0,0 +1,17 @@
+// Import express
+const express = require("express");
+
+// Create an instance of express
+const app = express();
+
+// Create a variable for the port
+const PORT = 3000;
+
+// Configure the app
+
+// Mount the middleware
+
+// Listen to the port
+app.listen(PORT, () => {
+  console.log(`Server running of port: ${PORT}`);
+});
